24c23a76903b7fa1589e6a2b07a2e5e39312c3fd
marioncandau <marioncandau.mc@gmail.com>
2020-12-07 18:36:50 +0100

    update #3

---
+++ api_fff.py
@@ -0,0 +1,132 @@
+#coding: utf-8
+from __future__ import unicode_literals
+import urllib.request
+import hashlib
+import time
+import famfoot_fff as famfoot
+import json
+
+def parse_coupe(id, slug, phase, poule):
+    listdates = []
+    link = "https://www.fff.fr/coupes/fff/national/" + str(id) + "-" + slug + "/phase-" + str(phase) + "/poule-" + str(poule) + "/derniers-resultats?showForm=true&use_automatic_dates=1&type=full"
+    req = urllib.request.Request(link, headers={'User-Agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+    response = urllib.request.urlopen(req)
+    html = str(response.read().decode('utf-8'))
+    list_dates = famfoot.previous_week_end()
+    for date in list_dates:
+        listdates.append(famfoot.retrieve_date(html, date, 1))
+
+    for date in listdates:
+        if(date != ""):
+            list_matchs = famfoot.gather_matchs(html, date, link, 1, 1) 
+            for match in list_matchs:
+
+
+
+def parse_champ(id, slug, phase, poule):
+    link = "https://www.fff.fr/championnats/fff/national/" + str(id) + "-" + slug + "/phase-" + str(phase) + "/poule-" + str(poule) + "/derniers-resultats?showForm=true&use_automatic_dates=1&type=full"
+    req = urllib.request.Request(link, headers={'User-Agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+    response = urllib.request.urlopen(req)
+    html = str(response.read().decode('utf-8'))
+    list_dates = famfoot.previous_week_end()
+
+
+def build_tab_res(html, url, g, bonnedate, champ_name, coupe, slug):
+    result = 0
+    c = []
+    b = 0
+    for date in bonnedate:
+        c.append(famfoot.retrieve_date(html, date, coupe))
+        if(c[-1] != ""):
+            result = 1
+    for date in c:
+        if(date != ""):
+            a = famfoot.gather_matchs(html, date, url, coupe, 1)
+            for t in a:
+                t.competition = champ_name
+                t.compet_slug = slug
+                g.write("{")
+                datetow = str(t.date.encode('utf-8'))[2:-1].replace("\\xc3\\xa9", "&eacute")
+                g.write('"date": "' + datetow + '",  ')
+                g.write('"journee": ' + str(t.journee) + ', ')
+                if(datetow not in datew):
+                    datew.append(datetow)
+                g.write('"numero": ' + str(t.numero) + ', ')
+                g.write('"equipe1": "' + str(t.equipe1.encode('utf-8'))[2:-1].replace("\\xc3\\xa9", "&eacute").upper().replace("\\XC3\\X89", "&Eacute").replace("\\XC3\\X88", "&Egrave").replace("\\xc3\\xa8", "&egrave") + '", ')
+                g.write('"equipe2": "' + str(t.equipe2.encode('utf-8'))[2:-1].replace("\\xc3\\xa9", "&eacute").upper().replace("\\XC3\\X89", "&Eacute").replace("\\XC3\\X88", "&Egrave").replace("\\xc3\\xa8", "&egrave") + '", ')
+                g.write('"equipe1_id": ' + str(t.equipe1_id) + ', ')
+                g.write('"equipe2_id": ' + str(t.equipe2_id) + ', ')
+                g.write('"score": "' + t.score + '", ')
+                g.write('"forfait1": ' + str(int(t.forfait1)) + ', ')
+                g.write('"forfait2": ' + str(int(t.forfait2)) + ', ')
+                g.write('"forfaitgeneral1": ' + str(int(t.forfaitgeneral1)) + ', ')
+                g.write('"forfaitgeneral2": ' + str(int(t.forfaitgeneral2)) + ', ')
+                g.write('"competition": "' + str(t.competition.encode('utf-8'))[2:-1].replace("\\xc3\\xa9", "&eacute").replace("\\xc3\\xa8", "&egrave") + '", ')
+                g.write('"compet_slug": "' + t.compet_slug + '", ')
+                g.write('"place1": ' + str(t.place1) + ', ')
+                g.write('"place2": ' + str(t.place2)+ ', ')
+                g.write('"lien": "' + t.lien[:-1] + '", ')
+                g.write('"coupe": ' + str(int(t.coupe)))
+                g.write("}\n")
+                
+f = open("resultats_famfoot.txt", "r", encoding = "utf-8")
+g = open("resultat.json", "w")
+datefile = open("date_resultat.json", "w")
+
+datew = []
+
+
+for line in f:
+    if(line[0] == '/'):
+        champ_name = line[3:-1]
+        response = 0
+        coupe = 0
+    elif(line[0] == '*'):
+        champ_name = line[3:-1]
+        coupe = 1
+        response = 0
+    elif(line[0] == 'C'):
+        h = open(line[:-1], "r")
+        response = h.readlines()
+        h.close()
+    elif(line[0] == '0'):
+        slug = line[1:-1]
+    elif(line[0] == 'h'):
+        try:
+            req = urllib.request.Request(line, headers={'User-Agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+            response = urllib.request.urlopen(req)
+        except:
+            print("erreur on line: " + line)
+            time.sleep(4)
+            try:
+                response = urllib.request.urlopen(req)
+            except:
+                if(datefile):
+                    datefile.write(str(datew))
+                    datefile.close()
+                raise
+        if(response != 0):
+            html = str(response.read().decode('utf-8'))
+            if(coupe == 1):
+                k = famfoot.retrieve_tour(html).replace("é", "&eacute")
+                champ_name = champ_name + " - " + k
+            g = open("resultat.json", "a")
+            bonnedate = famfoot.previous_week_end()
+            try:
+                build_tab_res(html, line, g, bonnedate, champ_name, coupe, slug)
+            except:
+                if(datefile):
+                    datefile.write(str(datew))
+                    datefile.close()
+                raise
+            g.close()
+
+if(datefile):
+    datefile.write(str(datew))
+    datefile.close()
+g.close()
+f.close()
+
+
+
+
---
+++ famfoot_fff.py
@@ -0,0 +1,242 @@
+#coding: utf-8
+from __future__ import unicode_literals
+import urllib.request
+import hashlib
+import time
+import datetime
+
+#<div class="score_match">
+score_match = "<div class=\"score_match\">" 
+title_box = "<div class=\"title_box\">"
+
+# Dico
+
+dico = {}
+dico["9121a1b22cfdd422236857422039efbd"] = 0 
+dico["8f0b1c0ec9577d2266f458e7e29a5d00"] = 1 
+dico["c5137151fc6a8fd42c407aa1c634042c"] = 2 
+dico["cfc3a46a2d9739062fe557cf7460107c"] = 3 
+dico["d15f59843a37d06eaa9f54c4dffc120e"] = 4 
+dico["3171f3b6d49576f436049ac22e8601bd"] = 5 
+dico["6a4e25f17c5a296199629371cf892ad9"] = 6 
+dico["88a82803d4dce6327428ee4a276f4b6e"] = 7 
+dico["483856166855aff3b439af5ace6f70da"] = 8 
+dico["f2e8d76f6abc1e2232f1e8a64b44f6bc"] = 9 
+
+tabjours = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+tabmois = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre']
+        
+
+def time_to_date(t):
+    if(t.tm_mday > 9):
+        return str((tabjours[t.tm_wday]+str(t.tm_mday)+tabmois[t.tm_mon-1]+str(t.tm_year)).encode('utf-8'))
+    else:
+        return str((tabjours[t.tm_wday]+"0" + str(t.tm_mday)+tabmois[t.tm_mon-1]+str(t.tm_year)).encode('utf-8')) 
+
+def next_week_end():
+    t2 = time.time()
+    t = time.localtime(t2)
+    if(t.tm_wday == 0):
+        bonnedate = [time_to_date(t), time_to_date(time.localtime(t2 + 86400)), time_to_date(time.localtime(t2 + 2*86400)), time_to_date(time.localtime(t2 + 3*86400)), time_to_date(time.localtime(t2 + 4*86400)), time_to_date(time.localtime(t2 + 5*86400)), time_to_date(time.localtime(t2 + 6*86400)), time_to_date(time.localtime(t2 + 7*86400))] 
+    elif(t.tm_wday == 1):
+        bonnedate = [time_to_date(time.localtime(t2 - 86400)), time_to_date(t), time_to_date(time.localtime(t2 + 86400)), time_to_date(time.localtime(t2 + 2*86400)), time_to_date(time.localtime(t2 + 3*86400)), time_to_date(time.localtime(t2 + 4*86400)), time_to_date(time.localtime(t2 + 5*86400)), time_to_date(time.localtime(t2 + 6*86400))]
+    elif(t.tm_wday == 2):
+        bonnedate = [time_to_date(time.localtime(t2 - 2*86400)), time_to_date(time.localtime(t2 - 86400)), time_to_date(t), time_to_date(time.localtime(t2 + 86400)), time_to_date(time.localtime(t2 + 2*86400)), time_to_date(time.localtime(t2 + 3*86400)), time_to_date(time.localtime(t2 + 4*86400)), time_to_date(time.localtime(t2 + 5*86400))]
+    elif(t.tm_wday == 3):
+        bonnedate = [time_to_date(time.localtime(t2 - 3*86400)), time_to_date(time.localtime(t2 - 2*86400)), time_to_date(time.localtime(t2 - 86400)), time_to_date(t), time_to_date(time.localtime(t2 + 86400)), time_to_date(time.localtime(t2 + 2*86400)), time_to_date(time.localtime(t2 + 3*86400)), time_to_date(time.localtime(t2 + 4*86400))]
+    elif(t.tm_wday == 4):
+        bonnedate = [time_to_date(time.localtime(t2 - 4*86400)),time_to_date(time.localtime(t2 - 3*86400)), time_to_date(time.localtime(t2 - 2*86400)), time_to_date(time.localtime(t2 - 86400)),time_to_date(t), time_to_date(time.localtime(t2 + 86400)), time_to_date(time.localtime(t2 + 2*86400)), time_to_date(time.localtime(t2 + 3*86400))]
+    elif(t.tm_wday == 5):     
+        bonnedate = [time_to_date(time.localtime(t2 - 5*86400)),time_to_date(time.localtime(t2 - 4*86400)), time_to_date(time.localtime(t2 - 3*86400)), time_to_date(time.localtime(t2 - 2*86400)),time_to_date(time.localtime(t2 - 86400)), time_to_date(t), time_to_date(time.localtime(t2 + 86400)), time_to_date(time.localtime(t2 + 2*86400))]
+    elif(t.tm_wday == 6):
+        bonnedate = [time_to_date(time.localtime(t2 - 6*86400)),time_to_date(time.localtime(t2 - 5*86400)), time_to_date(time.localtime(t2 - 4*86400)), time_to_date(time.localtime(t2 - 3*86400)),time_to_date(time.localtime(t2 - 2*86400)), time_to_date(time.localtime(t2 - 86400)), time_to_date(t), time_to_date(time.localtime(t2 + 86400))]
+
+    return bonnedate
+
+def previous_week_end():
+    t2 = time.time()
+    t = time.localtime(t2)
+    if(t.tm_wday == 0):
+        bonnedate = [time_to_date(time.localtime(t2)), time_to_date(time.localtime(t2 - 86400)), time_to_date(time.localtime(t2 - 2*86400)), time_to_date(time.localtime(t2 - 3*86400)), time_to_date(time.localtime(t2 - 4*86400)), time_to_date(time.localtime(t2 - 5*86400)), time_to_date(time.localtime(t2 - 6*86400)), time_to_date(time.localtime(t2 - 7*86400))] 
+    elif(t.tm_wday == 1):
+        bonnedate = [time_to_date(time.localtime(t2 - 86400)), time_to_date(time.localtime(t2 - 2*86400)), time_to_date(time.localtime(t2 - 3*86400)), time_to_date(time.localtime(t2 - 4*86400)), time_to_date(time.localtime(t2 - 5*86400)), time_to_date(time.localtime(t2 -6*86400)), time_to_date(time.localtime(t2 - 7*86400)), time_to_date(time.localtime(t2 - 8*86400))]
+    elif(t.tm_wday == 2):
+        bonnedate = [time_to_date(time.localtime(t2 - 2*86400)), time_to_date(time.localtime(t2 - 3*86400)), time_to_date(time.localtime(t2 - 4*86400)), time_to_date(time.localtime(t2 - 5*86400)), time_to_date(time.localtime(t2 - 6*86400)), time_to_date(time.localtime(t2 - 7*86400)), time_to_date(time.localtime(t2 - 8*86400)), time_to_date(time.localtime(t2 - 9*86400))]
+    elif(t.tm_wday == 3):
+        bonnedate = [time_to_date(time.localtime(t2 - 3*86400)), time_to_date(time.localtime(t2 - 4*86400)), time_to_date(time.localtime(t2 - 5*86400)), time_to_date(time.localtime(t2 - 6*86400)), time_to_date(time.localtime(t2 - 7*86400)), time_to_date(time.localtime(t2 - 8*86400)), time_to_date(time.localtime(t2 - 9*86400)), time_to_date(time.localtime(t2 - 10*86400))]
+    elif(t.tm_wday == 4):
+        bonnedate = [time_to_date(time.localtime(t2 - 4*86400)), time_to_date(time.localtime(t2 - 5*86400)),time_to_date(time.localtime(t2 - 6*86400)), time_to_date(time.localtime(t2 - 7*86400)), time_to_date(time.localtime(t2 - 8*86400)),time_to_date(time.localtime(t2 - 9*86400)), time_to_date(time.localtime(t2 - 10*86400)), time_to_date(time.localtime(t2 - 11*86400))]
+    elif(t.tm_wday == 5):         
+        bonnedate = [time_to_date(time.localtime(t2 - 5*86400)),time_to_date(time.localtime(t2 - 4*86400)), time_to_date(time.localtime(t2 - 3*86400)), time_to_date(time.localtime(t2 - 2*86400)),time_to_date(time.localtime(t2 - 86400)), time_to_date(t), time_to_date(time.localtime(t2 + 86400)), time_to_date(time.localtime(t2 + 2*86400))]
+    elif(t.tm_wday == 6):
+        bonnedate = [time_to_date(time.localtime(t2 - 6*86400)),time_to_date(time.localtime(t2 - 5*86400)), time_to_date(time.localtime(t2 - 4*86400)), time_to_date(time.localtime(t2 - 3*86400)),time_to_date(time.localtime(t2 - 2*86400)), time_to_date(time.localtime(t2 - 86400)), time_to_date(t), time_to_date(time.localtime(t2 + 86400))]
+
+    return bonnedate
+
+def gather_matchs(html, date, coupe):
+    all = dict()
+    calendrier = dict()
+    calendrier.journees = []
+    classement = dict()
+    agenda = dict()
+    resultats = dict()
+    matchs = []
+    journee_num = 0
+    journee_nom = ""
+
+    pos = html.find("<div id=\"match-list")
+    while(pos != -1):
+        pos1 = html.find(title_box, pos)
+        pos2 = html.find("<h4>", pos)
+        if pos1 < pos2:
+            if matchs != []:
+                journee = dict()
+                if coupe == 0:
+                    journee.numero = journee_num
+                else:
+                    journee.nom = journee_nom
+                journee.matchs = matchs
+                calendrier.journees.append(journee)
+            if coupe == 0:
+                journee_num = retrieve_journee(html, pos1)
+            else:
+                journee_nom = retrieve_tour(html, pos1)
+            pos = pos1
+        else:
+            date = parse_date(html, pos2)
+            pos = pos2
+
+        pos = html.find("eqleft", pos)
+        equipe_dom = parse_equipe(html, pos)
+        pos = html.find("class=\"score\"")
+        score = get_score(html, pos)
+        pos = html.find("eqright", pos)
+        equipe_ext = parse_equipe(html, pos)
+        pos = html.find("href=\"/match/")
+        match_id = get_id(html, pos)
+        match = dict()
+        match.date = dict()
+        match.date.lisible = date
+        match.date.timestamp = time.mktime(datetime.datetime.strptime(date, "%A %d %B %Y - %H:%M").timetuple())
+        match.equipes = dict()
+        match.equipes.dom = equipe_dom
+        match.equipes.ext = equipe_ext
+        match.score = score
+        match.id = match_id
+
+    all.calendrier = calendrier
+    all.classement = classement
+    all.resultats = resultats
+    all.agenda = agenda
+    return all
+
+
+def retrieve_date(page, bonnedate, coupe):
+    header=page.find("module-club")
+    if (coupe == 0):
+        pos=page.find("<br/></span><br/>",header) + len("<br/></span><br/>") + 11
+    else:
+        pos = page.find("<div class=\"date\">", header) + len("<div class=\"date\">") + 2
+    length = len(page)
+    date_current = ""
+    date_format = ""
+    i = pos
+    while(i < length and i != -1):
+        while(page[i] != '-' and page[i] != '<'):
+            if (date_current != "" or page[i] != " "):
+                date_current+=page[i]
+            if(page[i] != ' '):
+                date_format += page[i]
+            i = i + 1
+        if(coupe == 0):
+            pos=page.find("<br/></span><br/>", i)
+        else:
+            pos = page.find("<div class=\"date\">", i)
+        i = pos
+        if((str(date_format.replace("é", "e")) == str(str(bonnedate))[2:-1]) or (str(date_format.replace("û", "u")) == str(str(bonnedate))[2:-1])):
+            while(date_current[-1] == ' '):
+                date_current = date_current[:-1]
+            return date_current
+        else:
+            date_format = ""
+            date_current = ""
+        if(i == -1):
+            return ""
+        if(coupe == 0):
+            i = pos + len("<br/></span><br/>") + 2
+        else:
+            i = pos + len("<div class=\"date\">") + 2
+    return ""
+
+def retrieve_tour(page):
+    pos=page.find("<div class=\"date_tour\">") + len("<div class=\"date_tour\">") + 2
+    if(pos == len("<div class=\"date_tour\">") + 1):
+        return ""
+    pos1=page.find("</div>", pos)
+    tour = ""
+    i = pos
+    while(page[i] == " "):
+        i = i + 1
+    k = pos1-1
+    while(page[k] == " "):
+        k = k - 1
+    for j in range(i, k+1):
+        tour+=page[j]
+    
+    return tour
+
+def get_score(i, page):
+    tmp_score = "" 
+    upper_bound = page.find("</div>",i) 
+    http_pos_0 = http_pos_1 = 0 
+    ptr = i 
+    nb = 0 
+    # On récupère toutes les images.
+    while(http_pos_0 != -1):
+        old_http_pos_1 = http_pos_1 
+        http_pos_0 = page.find("http", ptr, upper_bound) 
+        http_pos_1 = page.find(".png", ptr, upper_bound) 
+        if http_pos_0 == -1:
+            if(tmp_score == ""):
+                tmp_score = " ?"
+            tmp_score += get_penalty(i, page)
+            return tmp_score
+        # On vérifie s'il y a un tiret entre les images
+        if nb >= 1 and page.find("-", old_http_pos_1, http_pos_0) != -1:
+            tmp_score += "-" 
+        current_img = page[http_pos_0:http_pos_1+4] 
+        ptr = http_pos_1+4 
+        # On charge l'image
+        file = urllib.request.urlopen(current_img).read() 
+        md5  = hashlib.md5() 
+        md5.update(file) 
+        hash_img = md5.hexdigest() 
+        nb+= 1 
+        tmp_score += str(dico[hash_img]) 
+
+    tmp_score += get_penalty(i, page)
+    return tmp_score
+
+def get_penalty(i, page):
+    penaltypos = page.find('<div class="score_match_penalty">',i) + len('<div class="score_match_penalty">')
+    while(page[penaltypos] == ' ' or page[penaltypos] == '\n'):
+        penaltypos+=1
+    endpenaltypos = page.find("</div>", penaltypos)
+    while(page[endpenaltypos-1] == ' '):
+        endpenaltypos-=1
+    penalty = page[penaltypos-1:endpenaltypos]
+    return penalty
+
+def retrieve_journee(linkmatch):
+    pos = linkmatch.find('Journ')+ len('Journee ')
+    pos1 = pos
+    while (linkmatch[pos1] != " "):
+        pos1 = pos1 + 1
+
+    jour = int(linkmatch[pos:pos1])
+    return(jour)
+    
+    
+
+
+
